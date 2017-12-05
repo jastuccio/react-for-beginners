26e8c56b5fc0770bb07f0736db90062f46a68ad6
joe <joe@astucc.io>
2017-12-05 09:10:38 -0500

    Adding React Router; Vid 11 complete

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <h2>Not Found!111!!</h2>;
+      <h2>Not Found!111!!</h2>
     )
   }
 }
